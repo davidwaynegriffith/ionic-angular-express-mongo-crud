1a7593ab1d97ac877278d863b8579e3d9561d7ab
David Griffith <davidwaynegriffith@gmail.com>
2022-04-17 05:19:41 -0700

    Adds Form

--- src/app/add-bean/add-bean.page.ts
+++ src/app/add-bean/add-bean.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { BeanService } from './../shared/bean.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder } from "@angular/forms";
+import { Bean } from '../shared/bean';
+import { timeStamp } from 'console';
 
 @Component({
   selector: 'app-add-bean',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-bean.page.scss'],
 })
 export class AddBeanPage implements OnInit {
+  beanForm: FormGroup;
+  id: any;
+  get isCreate():boolean {
+    return !this.id;
+  }
+  constructor(
+    private beanService: BeanService,
+    private router: Router,
+    private activeRoute: ActivatedRoute,
+    private fb: FormBuilder,
+    private zone: NgZone
+  ) {
 
-  constructor() { }
+    this.id = this.activeRoute.snapshot.paramMap.get('id') ? this.activeRoute.snapshot.paramMap.get('id') : false;
+
+  }
 
   ngOnInit() {
+    this.beanForm = this.fb.group([
+      name: [''],
+      type: ['']
+    ]);
+  }
+
+  onFormSubmit() {
+    if (!this.beanForm.valid) {
+      return false;
+    }
+    if (this.isCreate) {
+
+    this.beanService.addBean(this.beanForm.value)
+    .subscribe((res) => {
+     this.zone.run(() => {
+       console.log(res);
+       this.beanForm.reset();
+       this.router.navigate(['/home']);
+     })
+    });
+  } else {
+
+  }
   }
 
 }
